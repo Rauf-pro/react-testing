8965092e05aadc808c5f0ee28056f43f63c25f13
rrauf8178 <rrauf8178@gmail.com>
2024-08-28 20:58:49 +0400

    add: describe method separately too

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,15 +1,29 @@
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./greet";
 
+// Test suit grouping
+describe("Greet component", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
 
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText("Hello");
-  expect(textElement).toBeInTheDocument();
+  // Use nested  in group tests
+//   describe("Nested", () => {
+//     test("renders a name", () => {
+//       render(<Greet name="John" />);
+//       const textElement = screen.getByText("Hello John");
+//       expect(textElement).toBeInTheDocument();
+//     });
+//   });
 });
 
-test("Greet with a name", () => {
-  render(<Greet name="John" />);
-  const textElement = screen.getByText("Hello John");
-  expect(textElement).toBeInTheDocument();
-});
+// Or you can use the test suit grouping like this
+describe("Nested", () => {
+    test("renders a name", () => {
+      render(<Greet name="John" />);
+      const textElement = screen.getByText("Hello John");
+      expect(textElement).toBeInTheDocument();
+    });
+  });
