3554071811153da823a6c73e6ab8120efc94bc0a
rrauf8178 <rrauf8178@gmail.com>
2024-08-28 20:37:48 +0400

    add: second test passing props a name

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,8 +1,15 @@
 import { render, screen } from "@testing-library/react";
-import { Greet } from "../greet/Greet";
+import { Greet } from "./greet";
+
 
 test("Greet renders correctly", () => {
   render(<Greet />);
   const textElement = screen.getByText("Hello");
+  expect(textElement).toBeInTheDocument();
+});
+
+test("Greet with a name", () => {
+  render(<Greet name="John" />);
+  const textElement = screen.getByText("Hello John");
   expect(textElement).toBeInTheDocument();
 });
