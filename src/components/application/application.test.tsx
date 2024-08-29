7e11ed8ee673f1977e2789751c1e0ac602365cb7
rrauf8178 <rrauf8178@gmail.com>
2024-08-30 01:34:43 +0400

    add: test for custom element by data-testid with using getByTestId query method

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -31,6 +31,9 @@ describe("Application", () => {
     const imageElement = screen.getByAltText("a person with laptop");
     expect(imageElement).toBeInTheDocument();
 
+    const customElement = screen.getByTestId("custom-element");
+    expect(customElement).toBeInTheDocument();
+
     // or  using label
     // const nameElement2 = screen.getByLabelText("Name");
     // expect(nameElement2).toBeInTheDocument();
