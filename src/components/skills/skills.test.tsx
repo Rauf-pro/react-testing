9bee7c910ee080cbe6cc66d5ac7ba0982daa4fc7
rrauf8178 <rrauf8178@gmail.com>
2024-08-30 02:01:48 +0400

    add: test for all elements with getAllByRole method

--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -9,4 +9,10 @@ describe("Skills", () => {
     const listElement = screen.getByRole("list");
     expect(listElement).toBeInTheDocument();
   });
+
+  test("renders a list of skills", () => {
+    render(<Skills skills={skills} />);
+    const listElements = screen.getAllByRole("listitem");
+    expect(listElements).toHaveLength(skills.length);
+  })
 });
