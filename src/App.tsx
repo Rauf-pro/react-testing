d21f599801a4dcd9ae190f2dd70bc460f3cbac71
rrauf8178 <rrauf8178@gmail.com>
2024-08-30 20:05:51 +0400

    add: counter component for testing

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import './App.css';
 import { Application } from './components/application/application';
 import { Skills } from './components/skills/skills';
 import { SkillsProps } from './components/skills/skills.types';
+import { Counter } from './components/counter/counter';
 
 function App() {
   const skills = ["HTML", "CSS", "JavaScript"];
   return (
     <div className="App">
-      <Application/>
-      <Skills skills={skills}/>
+      {/* <Application/> */}
+      {/* <Skills skills={skills}/> */}
+      <Counter/>
     </div>
   );
 }
