49e6398303a160c21e5aa99d187b342f35aab837
rrauf8178 <rrauf8178@gmail.com>
2024-09-01 15:26:57 +0400

    add: mui-mode components by the material ui libruary and test for theme context

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import './App.css';
-import { Application } from './components/application/application';
-import { Skills } from './components/skills/skills';
-import { SkillsProps } from './components/skills/skills.types';
-import { Counter } from './components/counter/counter';
+import React from "react";
+import "./App.css";
+import { Application } from "./components/application/application";
+import { Skills } from "./components/skills/skills";
+import { Counter } from "./components/counter/counter";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { AppProviders } from "./providers/app-providers";
 
 function App() {
   const skills = ["HTML", "CSS", "JavaScript"];
   return (
-    <div className="App">
-      <Application/>
-      <Skills skills={skills}/>
-      <Counter/>
-      <div className='mb-5'></div>
-    </div>
+    <>
+      <div className="App">
+        <AppProviders>
+          <Application />
+          <Skills skills={skills} />
+          <Counter />
+
+          <div className="mb-5"></div>
+        </AppProviders>
+      </div>
+    </>
   );
 }
 
